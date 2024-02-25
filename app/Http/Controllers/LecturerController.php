4b79acdcf062d22ed3bbdcf55c622f7f663f812a
Alisher Kasimov <alisherkasimov@AKMBP.local>
2024-02-25 16:23:22 +0200

    Models, migrations, controllers and etc

---
+++ app/Http/Controllers/LecturerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLecturerRequest;
+use App\Http\Requests\UpdateLecturerRequest;
+use App\Models\Lecturer;
+
+class LecturerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLecturerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lecturer $lecturer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lecturer $lecturer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLecturerRequest $request, Lecturer $lecturer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lecturer $lecturer)
+    {
+        //
+    }
+}
